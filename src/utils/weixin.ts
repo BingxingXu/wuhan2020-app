7fbb77726f2c758fb01a2e9c38939494b7de644a
徐冰星 <xubingxing@minx.lan>
2020-02-04 19:49:07 +0800

    try fix

--- src/utils/weixin.ts
+++ src/utils/weixin.ts
@@ -13,13 +13,14 @@ export const initWeixin = async () => {
         const res: Config = await request(`/weixin/config`)
 
         wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx9ed52ed8359d7f25', // 必填，公众号的唯一标识
             timestamp: res.timestamp, // 必填，生成签名的时间戳
             nonceStr: res.nonceStr, // 必填，生成签名的随机串
             signature: res.signature,// 必填，签名
             jsApiList: [
-                'updateAppMessageShareData'
+                'updateAppMessageShareData',
+                'onMenuShareAppMessage'
             ] // 必填，需要使用的JS接口列表
         });
 
@@ -29,7 +30,7 @@ export const initWeixin = async () => {
         });
 
         wx.ready(function () {
-            wx.updateAppMessageShareData({
+            wx.onMenuShareAppMessage({
                 title: '抗击新冠肺炎  共同守卫平安', // 分享标题
                 desc: '汇总疫情动态，助您科学预防。武汉加油！中国平安！', // 分享描述
                 link: 'https://wuhan.90hub.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
