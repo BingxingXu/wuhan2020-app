4cb6d70809236f82a33c5861bf5aa8670ae27667
徐冰星 <xubingxing@minx.lan>
2020-02-05 12:46:28 +0800

    更新

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -353,10 +353,6 @@ class Index extends Component {
             {/* <text className="btn-hint">已有930,010,010人参与了祈福</text> */}
           </View>
         </View>
-        <Image
-          className="fab"
-          src="https://minx.oss-cn-shanghai.aliyuncs.com/wuhan/fab.png"
-        />
         <AtActionSheet isOpened={isActionOpen} cancelText='取消' title='点击发送吉祥语' onClose={closeAction}>
           {actionList.map((i, index) =>
             <AtActionSheetItem
